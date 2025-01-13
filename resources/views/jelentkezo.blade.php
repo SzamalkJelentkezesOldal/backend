5cc876e723605dcaaa38db7a84e192e25fec8298
dominikdobos <dominik.dobos33@gmail.com>
2025-01-13 18:13:48 +0100

    env módosítása cookie + emailhez, JelentkezoMail + view

---
+++ resources/views/jelentkezo.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jelentkezés</title>
+</head>
+<body>
+    <h1>Kedves {{$name}}!</h1>
+
+    <p>Ez egy teszt email....</p>
+
+    <h4><a href={{$url}} target="_blank">REGISZTRÁCIÓ</a></h4>
+</body>
+</html>
